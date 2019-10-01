fe34bb24c136b74eb2c94ef50aa30901a404848d
Mayki Hu <mayki@uw.edu>
2019-09-30 20:43:24 -0700

    Finish question 3, populates tables w/ 10 various restaurants

---
+++ hw1/submission/hw1-q3.sql
@@ -0,0 +1,17 @@
+CREATE TABLE MyRestaurants(
+        Name varchar(20),
+        Cuisine varchar(20),
+        Distance int,
+        LastVisit varchar(10),
+        Love int
+);
+INSERT INTO MyRestaurants VALUES('Pho Hoa', 'Vietnamese', 5, '2019-10-01', 1);
+INSERT INTO MyRestaurants VALUES('Canton Wonton House', 'Chinese', 5, '2019-09-29', 1);
+INSERT INTO MyRestaurants VALUES('Oasis Tea Zone', NULL, 5, '2019-09-30', NULL);
+INSERT INTO MyRestaurants VALUES('Seattle''s Best Tea', NULL, 5, '2019-09-09', 1);
+INSERT INTO MyRestaurants VALUES('Aladdin Gyro-Cery & Deli', 'Mediterranean', 30, '2019-09-20', 1);
+INSERT INTO MyRestaurants VALUES('Noodle Nation', 'Thai', 30, '2019-09-22', -1);
+INSERT INTO MyRestaurants VALUES('Mee Sum Pastry', 'Chinese', 30, '2019-05-15', -1);
+INSERT INTO MyRestaurants VALUES('Cafe On The Ave', NULL, 30, '2018-11-11', 1);
+INSERT INTO MyRestaurants VALUES('Young Tea', NULL, 5, '2019-10-01', 1);
+INSERT INTO MyRestaurants VALUES('Aloha Plates', 'Hawaiian', 5, '2019-09-06', 1);
